134e0b0c60114783d34579d5be82730d0693d529
frozentape <73592467+frozentape@users.noreply.github.com>
2022-03-19 12:13:28 +0000

    Add Wwise events for some of the music

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class AudioManager : GenericSingletonClass<AudioManager>
+{
+    public AK.Wwise.Bank MusicSoundBank;
+    public AK.Wwise.Event MusicStartEvent;
+    public AK.Wwise.Event KeyPickupEvent;
+    public AK.Wwise.Event ChangeLifeEvent;
+    public AK.Wwise.Event ExploringEvent;
+
+    public override void Awake()
+    {
+        base.Awake();
+
+        MusicSoundBank.Load();
+    }
+
+    private void Start()
+    {
+        MusicStartEvent.Post(gameObject);
+
+        // For now until I add menu.
+        ExploringEvent.Post(gameObject);
+    }
+}
